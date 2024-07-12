b1ff0db01b54770a58f3cfc2f212f233631cb69e
kimyoda <rladygks1210@naver.com>
2024-07-13 02:20:25 +0900

    [feat] weatherImages add, background color add

--- weaterapp/src/App.tsx
+++ weaterapp/src/App.tsx
@@ -1,23 +1,59 @@
 import React, { useEffect, useState } from "react";
+import CurrentWeather from "./components/CurrentWeather/CurrentWeater";
 import Loader from "./components/Loader/Loader";
+import SearchBar from "./components/SearchBar/SearchBar";
+import { getWeather } from "./services/WeatherService";
 import "./App.css";
 
 import sun from "../../weaterapp/src/assets/images/sun.jpg";
-import SearchBar from "./components/SearchBar/SearchBar";
-import CurrentWeather from "./components/CurrentWeather/CurrentWeater";
-import { getWeather } from "./services/WeatherService";
+import clouds from "../../weaterapp/src/assets/images/clouds.jpg";
+import fog from "../../weaterapp/src/assets/images/fog.jpg";
+import rain from "../../weaterapp/src/assets/images/rain.jpg";
+import sky from "../../weaterapp/src/assets/images/sky.jpg";
+import snow from "../../weaterapp/src/assets/images/snow.jpg";
+import thunder from "../../weaterapp/src/assets/images/thunder.jpg";
 
 const App = () => {
-  const [isLoding, setIsLoding] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [data, setData] = useState<any>(null);
 
+  const weatherImages: Record<string, string> = {
+    "clear sky": sky,
+    "few clouds": clouds,
+    "scattered clouds": clouds,
+    "partly cloudy": clouds,
+    "overcast cloudy": clouds,
+    "broken clouds": clouds,
+    "light rain": rain,
+    "moderate rain": rain,
+    "heavy rain": rain,
+    showers: rain,
+    thunderstorm: thunder,
+    snow: snow,
+    mist: fog,
+    fog: fog,
+    haze: fog,
+    smoke: fog,
+  };
+
   const generateBackgroundStyle: { backgroundImage: string } = {
-    backgroundImage: `url(${sun})`,
+    backgroundImage: `url(${weatherImages[data?.weather[0]?.description] || sun})`,
+  };
+
+  const handleSearch = (city: string) => {
+    getWeather(city)
+      .then((result) => {
+        setData(result.data);
+      })
+      .catch(console.error)
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   useEffect(() => {
     const defaultCity = "Seoul";
-    setIsLoding(true);
+    setIsLoading(true);
 
     getWeather(defaultCity)
       .then((result) => {
@@ -25,16 +61,16 @@ const App = () => {
       })
       .catch(console.error)
       .finally(() => {
-        setIsLoding(false);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <div className="weather-app">
-      {isLoding && <Loader />}
+      {isLoading && <Loader />}
       <div className="background" style={generateBackgroundStyle}></div>
       <div className="container">
-        <SearchBar onSearchClick={() => {}} />
+        <SearchBar onSearchClick={handleSearch} />
         <CurrentWeather data={data} />
       </div>
     </div>
